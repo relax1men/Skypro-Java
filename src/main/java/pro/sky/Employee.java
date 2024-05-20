8df41770ed438d575602747417e30d8faaea7d91
relax1men <dmitrden90@gmail.com>
2024-05-20 20:09:02 +0300

    hw 2.8

--- src/main/java/pro/sky/Employee.java
+++ src/main/java/pro/sky/Employee.java
@@ -12,14 +12,17 @@ public class Employee {
     }
 
     public String getFirstName() {
+
         return firstName;
     }
 
     public void setFirstName(String firstName) {
+
         this.firstName = firstName;
     }
 
     public String getLastName() {
+
         return lastName;
     }
 
