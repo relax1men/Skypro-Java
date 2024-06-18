ebabf44686bfa8fe56cafb028d9aa243f643064f
relax1men <dmitrden90@gmail.com>
2024-06-18 14:57:38 +0300

    hw 2.11

---
+++ src/main/java/pro/sky/model/Item.java
@@ -0,0 +1,18 @@
+package pro.sky.model;
+public class Item {
+    private final int id;
+    private final String title;
+
+    public Item(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
